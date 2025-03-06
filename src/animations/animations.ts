44729e4623575e2f9dd50d3b3cc6caf97d322395
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-03-06 01:27:17 +0100

    first commit

--- src/animations/animations.ts
+++ src/animations/animations.ts
@@ -3,5 +3,5 @@ import { gsap } from "gsap";
 // Make the text move right
 export const slideRight = (target: string | HTMLElement | null) => {
   if (!target) return;
-  gsap.to(target, { x: 400, duration: 3, opacity: 0.5 });
+  gsap.to(target, { x: 400, duration: 3, opacity: 0.5, delay:2 });
 };
