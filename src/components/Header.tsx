a957d7f725f9e641fb4bf0e2773f89e204abea21
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-03-07 22:33:45 +0100

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import HeaderWrapper from "../styles/HeaderWrapper.ts";
+
+function Header() {
+  return (
+    <HeaderWrapper>
+      <div className="container">
+        <h3>This is the header</h3>
+      </div>
+    </HeaderWrapper>
+  );
+}
+
+export default Header;
