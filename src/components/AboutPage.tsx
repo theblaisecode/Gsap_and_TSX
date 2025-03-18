630f1dfca7bbd933b316ae29f40130b03b5fa43c
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-03-18 19:05:25 +0100

    first commit

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -1,5 +1,31 @@
+import bentoImg from "../assets/TeddyTheCreator.png";
+
 export const BentoGrid = () => {
-  return <h2>Bento Grid</h2>;
+  return (
+    <div className="bentoGrid">
+      <div className="item">
+        <img src={bentoImg} alt="TeddyTheCreator" />
+      </div>
+
+      <div className="item aboutTeddy">
+        <h2>
+          <span className="highlight">About</span> Teddy
+        </h2>
+
+        <div className="aboutTeddyDescriptions">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure fugit
+            nemo provident ut corrupti quo recusandae quasi voluptas saepe
+            totam.
+          </p>
+        </div>
+      </div>
+
+      <div className="item testimonial">
+
+      </div>
+    </div>
+  );
 };
 
 export const Experience = () => {
--- src/styles/AboutWrapper.ts
+++ src/styles/AboutWrapper.ts
@@ -7,6 +7,48 @@ const AboutWrapper = styled.section`
     align-items: center;
     gap: 3rem;
   }
+
+  /* Bento Grid */
+  .bentoGrid {
+    width: 100%;
+    display: grid;
+    gap: 1.5rem;
+    padding: 1rem;
+    grid-template-columns: auto auto auto;
+    grid-template-rows: auto;
+    grid-template-areas:
+      "image description description"
+      "testimonial testimonial testimonial";
+    /* overflow: hidden; */
+  }
+
+  .item {
+    padding: 2rem;
+    border: 2px solid #464545;
+    border-radius: 5px;
+    /* max-width: 500px; Adjust as needed */
+    /* width: fit-content; Shrinks to fit content */
+    /* width: max-content; Makes the width fit the content */
+    /* align-self: start; Prevents stretching */
+  }
+
+  .bentoGrid .item:nth-child(1) {
+    grid-area: image;
+    max-width: 300px;
+  }
+  
+  .bentoGrid .item:nth-child(2) {
+    max-width: 600px;
+    grid-area: description;
+  }
+  
+  .bentoGrid .item:nth-child(3) {
+    grid-area: testimonial;
+  }
+
+  .bentoGrid img {
+    width: 100%;
+  }
 `;
 
 export default AboutWrapper;
