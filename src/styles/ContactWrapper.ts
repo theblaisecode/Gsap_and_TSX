ca966fb045ce8d1c74420f2f1e3285bd1ab0bf8b
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-03-24 20:35:53 +0100

    first commit

--- src/styles/ContactWrapper.ts
+++ src/styles/ContactWrapper.ts
@@ -8,7 +8,7 @@ const ContactWrapper = styled.section`
     align-items: center;
     /* border: 2px solid #464545;
     border-radius: 5px; */
-    padding: 2rem;
+    padding: 5rem;
     background: rgba(204, 17, 0, 0.1);
     box-shadow: 0 0.8rem 3.2rem 0 rgba(204, 38, 0, 0.08);
     backdrop-filter: blur(0.65rem);
@@ -22,40 +22,72 @@ const ContactWrapper = styled.section`
     flex: 1;
   }
 
-  .letsWorkContent {
+  .letsWorkContent,
+  .letsWorkContent .contentTop {
     display: flex;
     flex-direction: column;
-    gap: 3rem;
+    gap: 5rem;
+  }
+
+  .letsWorkContent .contentTop {
+    gap: 2rem;
+  }
+
+  .letsWorkContent .contentTop h2 {
+    font-size: 4rem;
+    font-weight: 500;
+  }
+
+  .letsWorkContent .contentTop p {
+    opacity: 0.7;
+    line-height: 3rem;
+    letter-spacing: 0.07rem;
+  }
+
+  .letsWorkContent .contentBottom {
+    width: 70%;
   }
 
   .contactSocials {
-    display: flex;
-    gap: 1rem;
-  }
-
-  .contactSocials .contentBottom {
-    display: inline-block;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 3rem;
   }
 
   .contactSocials a {
-    /* display: flex;
+    display: flex;
     justify-content: flex-start;
     gap: 2rem;
-    align-items: center; */
+    align-items: center;
+    color: var(--textLight);
+    transition: 0.2s;
   }
 
-  .contactSocials a span{
+  .contactSocials a span {
     display: inline-block;
+    transition: 0.4s;
   }
 
   .contactSocials a .icon {
     background: var(--accent);
     color: var(--textLight);
+    text-align: center;
     padding: 1rem;
     font-size: 2rem;
     width: 5rem;
     height: 5rem;
     border-radius: 50%;
+    transition: 0.4s;
+  }
+
+  .contactSocials a:hover {
+    color: var(--primary);
+    transition: all 0.2s;
+  }
+
+  .contactSocials a:hover .icon {
+    background: var(--primary);
+    transition: all 0.4s;
   }
 `;
 
